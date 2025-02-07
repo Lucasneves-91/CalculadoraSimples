fab91346d6519772775ca9b8afda527ea06eb3e6
Lucas Neves <lucas.ccdr@gmail.com>
2025-02-07 16:52:05 -0300

    Solicitação dos numeros para o usuario

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
+/** Objetivo: Criar uma calculadora que faz soma, multiplicação, divisão e subtração.
+ *
+ * 1 - Pedir dois numeros para o usuario (double)
+ * 2 - Apresentar uma tabela/texto para escolher a operação
+ * 3 - Resgatar a operação que o usuário selecionou
+ * 4 - Realizar o calculo
+ * 5 - Exibir resultado
+ * 6 - Se a operação for inválido, exibir mensagem de erro
+ * 7 - Se a divisão for por 0, exibir mensagem de erro
+ */
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        double numero1;
+        double numero2;
+
+        System.out.println("Digite o primeiro número: ");
+        numero1 = scanner.nextDouble();
+
+        System.out.println("Digite o segundo número: ");
+        numero2 = scanner.nextDouble();
+
+        System.out.println(numero1 + numero2);
     }
 }
