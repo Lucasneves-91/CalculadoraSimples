8c9b5e66282d3d034efba2b35d5b5fff90213985
Lucas Neves <lucas.ccdr@gmail.com>
2025-02-11 19:41:01 -0300

    Incluido cálculos, exibição dos resultados e opção caso seja inválida a opção.

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,12 @@
 import java.util.Scanner;
 
 /** Objetivo: Criar uma calculadora que faz soma, multiplicação, divisão e subtração.
- *
  * 1 - Pedir dois numeros para o usuario (double) - ok
  * 2 - Apresentar uma tabela/texto para escolher a operação - ok
- * 3 - Resgatar a operação que o usuário selecionou
- * 4 - Realizar o calculo
- * 5 - Exibir resultado
- * 6 - Se a operação for inválido, exibir mensagem de erro
- * 7 - Se a divisão for por 0, exibir mensagem de erro
+ * 3 - Resgatar a operação que o usuário selecionou - ok
+ * 4 - Realizar o calculo - ok
+ * 5 - Exibir resultado - ok
+ * 6 - Se a operação for inválido, exibir mensagem de erro - ok
  */
 
 public class Main {
@@ -17,6 +15,16 @@ public class Main {
 
         double numero1;
         double numero2;
+        double resultado;
+        int opcao;
+        String m = """
+                Qual operação você quer realizar:
+                1 - Soma
+                2 - Subtração
+                3 - Divisão
+                4 - Multiplicação
+                5 - Sair
+                """;
 
         System.out.println("Digite o primeiro número: ");
         numero1 = scanner.nextDouble();
@@ -24,16 +32,33 @@ public class Main {
         System.out.println("Digite o segundo número: ");
         numero2 = scanner.nextDouble();
 
-        System.out.println(numero1 + numero2);
-        menu();
-    }
+        while (true){
+            System.out.println(m);
+            opcao = scanner.nextInt();
+            if (opcao == 1) {
+                resultado = numero1 + numero2;
+                System.out.println("O resultado da soma é: " + resultado);
+                break;
+            } else if (opcao == 2) {
+                resultado = numero1 - numero2;
+                System.out.println("O resultado da subtração é: " + resultado);
+                break;
+            } else if (opcao == 3) {
+                resultado = numero1 / numero2;
+                System.out.println("O resultado da divisão é: " + resultado);
+                break;
+            } else if (opcao == 4) {
+                resultado = numero1 * numero2;
+                System.out.println("O resultado da multiplicação é: " + resultado);
+                break;
+            }else if (opcao == 5) {
+                System.out.println("Obrigado!");
+                break;
+            } else {
+                System.out.println("Opção inválida! Escolha uma opção válida");
+            }
+        }
 
-    public static String menu (){
-        System.out.println("Qual operação você quer realizar:");
-        System.out.println("1 - soma");
-        System.out.println("2 - subtração");
-        System.out.println("3 - divisão");
-        System.out.println("4 - multiplicação");
-        return "";
+
     }
 }
